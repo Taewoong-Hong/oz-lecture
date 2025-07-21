9bebf4d77658b0d0f3c4a5f813837dcc63cefd70
Taewoong-Hong <ghdxodnd@gmail.com>
2025-07-21 15:35:50 +0900

    add ^

--- day18/calculator/js/operations.js
+++ day18/calculator/js/operations.js
@@ -12,6 +12,7 @@ export default function calculateOperation(firstNumber, secondNumber, operator) 
         throw new Error("0으로 나눌 수 없습니다.");
       }
       return firstNumber / secondNumber;
+    case '^': return firstNumber ** secondNumber; // 제곱 연산 추가
     default:
       throw new Error(`알 수 없는 연산자입니다: ${operator}`);
   }
